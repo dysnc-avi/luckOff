22e610ef53d281cb36c147e898efe73c8fb75ef3
Abhinav <abhinav.contact22@gmail.com>
2025-02-23 05:10:48 +0530

    Added switch case entry point

--- src/games/roulette/main.c
+++ src/games/roulette/main.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include "nums.h"
+#include <stdbool.h>
 
 int main(){
-  char c;
- printf("Hello World\n");
- printf("Enter a character: ");
- scanf("%c", &c);
- print_num(c);
- return 0;
+  int choice;
+  bool loop = true;
+  printf("Welcome to the game\n");
+ while (loop==true)
+ {
+  printf("Press 1 for rules, 2 to play, 3 to exit\n");
+  scanf("%d", &choice);
+  switch (choice){
+    case 1:
+      printf("This is rule\n");
+      break;
+    case 2:
+    // todo: Add game logic
+      printf("This is play\n");
+      loop = false;
+      break;
+    case 3:
+      printf("See you later\n");
+      loop = false;
+      break;
+    default: 
+      printf("Invalid choice\n");
+      loop = false;
+      break;
+  }
+ }
 }
